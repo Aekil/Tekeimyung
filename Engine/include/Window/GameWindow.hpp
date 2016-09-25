fb56f13a3dd1db571af7ed757532ff8a7ef47998
Julien <julien.chardon@epitech.eu>
2016-09-25 23:42:00 +0900

    Merge fix

--- Engine/include/Window/GameWindow.hpp
+++ Engine/include/Window/GameWindow.hpp
@@ -11,8 +11,7 @@ class GameWindow
 {
 public:
     explicit    GameWindow(int width = WINDOW_DEFAULT_WIDTH, int height = WINDOW_DEFAULT_HEIGHT, const char *title = WINDOW_DEFAULT_TITLE);
-<<<<<<< HEAD
-    virtual	~GameWindow();
+    virtual     ~GameWindow();
     
     int         initialize();
     
@@ -23,30 +22,12 @@ public:
     void        setDecorated(bool decorated);
     void        setMaximized(bool fullscreen);
     void        setResizable(bool resizable);
-    
-=======
-                ~GameWindow();
 
-    int         initialize();
-
-    int         width() const;
-    int         height() const;
-    std::string name() const;
-
-    void        setDecorated(bool decorated);
-    void        setMaximized(bool fullscreen);
-    void        setResizable(bool resizable);
-
->>>>>>> 283ac34c1adff2944fe0e528fd81d98014c88b39
     bool        isRunning() const;
     void        display();
     void        pollEvents();
     void        close();
-<<<<<<< HEAD
     
-=======
-
->>>>>>> 283ac34c1adff2944fe0e528fd81d98014c88b39
 private:
     GLFWwindow  *_window;
 
