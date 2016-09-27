b776fea1995da11f6da05531324bb244c7c15f6f
Nokitoo <guillaume.labey@epitech.eu>
2016-09-27 11:08:41 +0900

    add System base class

---
+++ Engine/include/Core/System.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <functional>
+#include <typeindex>
+#include "EntityManager.hpp"
+#include "Components.hh"
+
+class System
+{
+public:
+    System();
+    virtual ~System();
+
+    virtual void        update(EntityManager& em, float elapsedTime) = 0;
+    virtual bool        init();
+    void                forEachEntity(EntityManager& em, std::function<void (Entity* entity)> callback);
+
+    template<typename ComponentType>
+    void addDependency()
+    {
+        _components.push_back(std::type_index(typeid(ComponentType)));
+    }
+
+
+protected:
+    // Store components type_index (copyable wrapper of type_info)
+    std::vector<std::type_index>         _components;
+};
