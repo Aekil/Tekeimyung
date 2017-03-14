b3ad3919ab5f1cee4366ebf2e64b31151f803ada
Nokitoo <guillaume.labey@epitech.eu>
2017-03-14 19:24:24 +0900

    Revert modification on System::forEachEntity

--- ECS/src/ECS/System.cpp
+++ ECS/src/ECS/System.cpp
@@ -17,8 +17,7 @@ bool    System::init()
 
 void    System::forEachEntity(EntityManager& em, std::function<void(Entity* entity)> callback)
 {
-    uint32_t entitiesNb = static_cast<uint32_t>(this->_entities.size());
-    for (uint32_t idx = 0; idx < entitiesNb; idx++)
+    for (uint32_t idx = 0; idx < this->_entities.size(); idx++)
     {
         Entity* entity = em.getEntity(this->_entities[idx]);
         if (entity)
