b70a9fae44a9f0ad460bda4fc8b86c9dd582e486
Nokitoo <guillaume.labey@epitech.eu>
2017-03-30 12:58:42 +0900

    traps deal damage

--- Game/src/Game/Scripts/Trap.cpp
+++ Game/src/Game/Scripts/Trap.cpp
@@ -4,9 +4,16 @@
 
 #include <Engine/Components.hh>
 
+#include <Game/Scripts/Enemy.hpp>
 #include <Game/Scripts/Trap.hpp>
 
 void Trap::onCollisionEnter(Entity* entity)
 {
+    if (entity->getTag() == "Enemy")
+    {
+        sScriptComponent* script = entity->getComponent<sScriptComponent>();
+        Enemy* enemy = script ? script->getScript<Enemy>("Enemy") : nullptr;
 
+        enemy->takeDamage(this->damage);
+    }
 }
