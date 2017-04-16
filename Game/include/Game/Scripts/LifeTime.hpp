a605c5b608b331c1b0bdf706afb603179dd54e3a
Simon AMBROISE <simonambroise.dev@gmail.com>
2017-04-16 19:25:31 +0900

    Adding lifetime script

---
+++ Game/include/Game/Scripts/LifeTime.hpp
@@ -0,0 +1,29 @@
+/*
+** @Author : Simon AMBROISE
+*/
+
+#pragma once
+
+#include <Engine/Core/BaseScript.hpp>
+#include <Engine/Core/ScriptFactory.hpp>
+
+class LifeTime final : public BaseScript
+{
+private:
+    float _lifeTime;
+    float _elapsedTime;
+
+public:
+    LifeTime() = default;
+    ~LifeTime() = default;
+
+    void start() override final;
+    void update(float dt) override final;
+
+public:
+    bool updateEditor() override final;
+    JsonValue saveToJson() override final;
+    void loadFromJson(const JsonValue& json) override final;
+};
+
+REGISTER_SCRIPT(LifeTime);
---
+++ Game/src/Game/Scripts/LifeTime.cpp
@@ -0,0 +1,44 @@
+/*
+** @Author : Simon AMBROISE
+*/
+
+#include <Game/Scripts/LifeTime.hpp>
+
+void LifeTime::start()
+{
+}
+
+void LifeTime::update(float dt)
+{
+    if (this->_lifeTime == 0.0f)
+        return;
+
+    this->_elapsedTime += dt;
+
+    if (this->_elapsedTime > this->_lifeTime)
+        this->Destroy();
+}
+
+bool LifeTime::updateEditor()
+{
+    bool changed = false;
+
+    ImGui::Text("Lifetime for entity (0 is disabled)");
+
+    changed |= ImGui::InputFloat("LifeTime", &(this->_lifeTime), 0.0f, ImGuiInputTextFlags_AllowTabInput);
+    
+    return (changed);
+}
+
+JsonValue LifeTime::saveToJson()
+{
+    JsonValue json;
+    
+    json.setFloat("lifetime", this->_lifeTime);
+    return (json);
+}
+
+void LifeTime::loadFromJson(const JsonValue& json)
+{
+    this->_lifeTime = json.getFloat("lifetime", 0.0f);
+}
