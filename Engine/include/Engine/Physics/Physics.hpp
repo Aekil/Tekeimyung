01f2e4b303ffb823842d0dc3183ddc59874bb892
Simon AMBROISE <simonambroise.dev@gmail.com>
2017-04-18 17:35:58 +0900

    Add filter to RaycastAll

--- Engine/include/Engine/Physics/Physics.hpp
+++ Engine/include/Engine/Physics/Physics.hpp
@@ -18,6 +18,8 @@ public:
     static bool raycast(const Ray& ray, Entity** hitEntity, std::vector<Entity*> entitiesFilter, bool useRange);
     static bool raycast(const Ray& ray, Entity** hitEntity);
     static bool raycastAll(const Ray& ray, std::vector<Entity*>& hitEntities);
+    static bool raycastAll(const Ray& ray, std::vector<Entity*>& hitEntities, std::vector<Entity*> entitiesFilter, bool useRange);
+
     static bool raycastPlane(const Ray& ray, const glm::vec3& planeNormal, const glm::vec3& planePos, float& hitDistance);
 
     static float    distance(const glm::vec3& origin, const glm::vec3& position);
