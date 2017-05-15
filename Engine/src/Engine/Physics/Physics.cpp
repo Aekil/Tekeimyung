bfa139ca9db4fd32d18c4d7c731f50bdd085cb56
Nokitoo <guillaume.labey@epitech.eu>
2017-05-15 16:33:04 +0900

    Remove useless Physics::raycast/Physics::raycastAll overloads + pass entitesFilter by reference

--- Engine/src/Engine/Physics/Physics.cpp
+++ Engine/src/Engine/Physics/Physics.cpp
@@ -12,7 +12,10 @@ Physics::Physics() {}
 
 Physics::~Physics() {}
 
-bool        Physics::raycast(const Ray& ray, Entity** hitEntity, std::vector<Entity*> entitiesFilter, float range)
+bool        Physics::raycast(const Ray& ray,
+                            Entity** hitEntity,
+                            const std::vector<Entity*>& entitiesFilter,
+                            float range)
 {
     float                   nearestHitDist = 0.0f;
     Entity*                 nearestEntity = nullptr;
@@ -66,45 +69,10 @@ bool        Physics::raycast(const Ray& ray, Entity** hitEntity, std::vector<Ent
     return (nearestEntity != nullptr);
 }
 
-bool    Physics::raycast(const Ray& ray, Entity** hitEntity)
-{
-    float nearestHitDist = 0.0f;
-    Entity* nearestEntity = nullptr;
-
-    EntityManager* em = EntityFactory::getBindedEntityManager();
-    for (Entity* entity: em->getEntities())
-    {
-        sRenderComponent* render = entity->getComponent<sRenderComponent>();
-        sTransformComponent* transform = entity->getComponent<sTransformComponent>();
-
-        // We can't select entity that is not displayed or has model not initialized
-        if (!render || !render->getModel() || render->ignoreRaycast)
-            continue;
-
-        // Model box collider position
-        glm::vec3 boxMin = glm::vec3(render->getModel()->getMin().x, render->getModel()->getMin().y, render->getModel()->getMin().z);
-        glm::vec3 boxMax = glm::vec3(render->getModel()->getMax().x, render->getModel()->getMax().y, render->getModel()->getMax().z);
-
-        boxMin *= transform->getScale();
-        boxMax *= transform->getScale();
-
-        // Convert box collider to world position
-        boxMin += transform->getPos();
-        boxMax += transform->getPos();
-
-        float distance = Collisions::rayVSAABB(ray, boxMin, boxMax);
-        if (distance != 0 && (nearestEntity == nullptr || distance <= nearestHitDist))
-        {
-            nearestHitDist = distance;
-            nearestEntity = entity;
-        }
-    }
-
-    *hitEntity = nearestEntity;
-    return (nearestEntity != nullptr);
-}
-
-bool    Physics::raycastAll(const Ray& ray, std::vector<Entity*>& hitEntities, std::vector<Entity*> entitiesFilter, float range)
+bool    Physics::raycastAll(const Ray& ray,
+                            std::vector<Entity*>& hitEntities,
+                            const std::vector<Entity*>& entitiesFilter,
+                            float range)
 {
     EntityManager* em = EntityFactory::getBindedEntityManager();
     for (Entity* entity : em->getEntities())
@@ -145,39 +113,6 @@ bool    Physics::raycastAll(const Ray& ray, std::vector<Entity*>& hitEntities, s
                     hitEntities.push_back(entity);
                 }
             }
-        }
-    }
-
-    return (hitEntities.size() > 0);
-}
-
-bool    Physics::raycastAll(const Ray& ray, std::vector<Entity*>& hitEntities)
-{
-    EntityManager* em = EntityFactory::getBindedEntityManager();
-    for (Entity* entity: em->getEntities())
-    {
-        sRenderComponent* render = entity->getComponent<sRenderComponent>();
-        sTransformComponent* transform = entity->getComponent<sTransformComponent>();
-
-        // We can't select entity that is not displayed or has model not initialized
-        if (!render || !render->getModel() || render->ignoreRaycast)
-            continue;
-
-        // Model box collider position
-        glm::vec3 boxMin = glm::vec3(render->getModel()->getMin().x, render->getModel()->getMin().y, render->getModel()->getMin().z);
-        glm::vec3 boxMax = glm::vec3(render->getModel()->getMax().x, render->getModel()->getMax().y, render->getModel()->getMax().z);
-
-        boxMin *= transform->getScale();
-        boxMax *= transform->getScale();
-
-        // Convert box collider to world position
-        boxMin += transform->getPos();
-        boxMax += transform->getPos();
-
-        float distance = Collisions::rayVSAABB(ray, boxMin, boxMax);
-        if (distance != 0)
-        {
-            hitEntities.push_back(entity);
         }
     }
 
